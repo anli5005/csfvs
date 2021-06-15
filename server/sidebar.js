f374983509aeb5334540039cd739d93e2204913f
Edward Feng <ed@ewf.me>
2021-06-15 14:21:12 -0400

    Add list review queries

--- server/sidebar.js
+++ server/sidebar.js
@@ -20,3 +20,13 @@ export async function getSidebarDetails(db) {
         }, [])
     };
 }
+
+export async function getUserReviews(db, user) {
+    const res = await db.query("SELECT project_id FROM reviews WHERE user_id = $1;", [user.user_id]);
+    return res.rows;
+}
+
+export async function getReviewCounts(db) {
+    const res = await db.query("SELECT r.project_id, COUNT(r.review_id) as all, SUM(CAST(u.type='judge' AS integer)) as judge FROM reviews as r, users as u WHERE r.user_id = u.user_id GROUP BY r.project_id;");
+    return res.rows;
+}
